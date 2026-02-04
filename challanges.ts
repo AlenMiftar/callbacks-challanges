54ce98ab7e60db60c58d9858772a560f300d4bb2
Alen Miftar <alen.miftar@dev95.se>
2026-02-04 12:00:38 +0100

    created a function that accepts one input and adds 2 to it

---
+++ challanges.ts
@@ -0,0 +1,12 @@
+// Challenge 1 Done ✔️
+/*  Create a function addTwo that accepts one input and adds 2 to it. */
+
+// To check if you've completed it, uncomment these console.logs!
+// console.log(addTwo(3));
+// console.log(addTwo(10));
+
+const addTwo = (input: number) => {
+  return input + 2;
+};
+console.log(addTwo(3));
+console.log(addTwo(10));
